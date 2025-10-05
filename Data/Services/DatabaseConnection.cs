08d2bbadcccc3bf19380bd796d8aa142099d464a
gabriel <patosquare418@gmail.com>
2025-10-05 00:02:46 -0400

    feat: singleton database connection

--- Data/Services/DatabaseConnection.cs
+++ Data/Services/DatabaseConnection.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+
 namespace FuerzaG.Data.Services;
 
 public class DatabaseConnection
@@ -5,9 +7,31 @@ public class DatabaseConnection
     private static DatabaseConnection _instance;
     private static readonly object _locker = new object();
     private readonly string _connectionString;
+    private readonly IConfiguration _configuration;
 
-    private DatabaseConnection()
+    private DatabaseConnection(IConfiguration configuration)
     {
-        _connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("NeonPostgres")!;
+    }
+
+    public static DatabaseConnection GetInstance(IConfiguration configuration)
+    {
+        if (_instance == null)
+        {
+            lock (_locker)
+            {
+                if (_instance == null)
+                {
+                    _instance = new DatabaseConnection(configuration);
+                }
+            }
+        }
+        return _instance;
+    }
+
+    public NpgsqlConnection GetConnection()
+    {
+        return new NpgsqlConnection(_connectionString);
     }
 }
