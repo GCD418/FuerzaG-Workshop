b05233166a488cf7dbd73a3f6a9053c7a87bdb86
gabriel <patosquare418@gmail.com>
2025-10-05 00:19:04 -0400

    feat: functional Singleton Database

---
+++ Pages/Prueba.cshtml.cs
@@ -0,0 +1,34 @@
+using System.Data;
+using FuerzaG.Data.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Npgsql;
+
+namespace FuerzaG.Pages;
+
+public class Prueba : PageModel
+{
+    private readonly IConfiguration _configuration;
+    public DataTable CategoriasTable { get; set; } = new DataTable();
+    
+    public Prueba(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public void OnGet()
+    {
+        Select();
+    }
+
+    void Select()
+    {
+        var database = DatabaseConnection.GetInstance(_configuration);
+        string query = @"SELECT * FROM ""Brand""";
+        using (NpgsqlConnection connection = database.GetConnection())
+        {
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+            connection.Open();
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command);
+            adapter.Fill(CategoriasTable);
+        }
+    }
+}
