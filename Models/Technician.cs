eefeea840a3fe7eb730ab0bd63082945936d4f81
gabriel <patosquare418@gmail.com>
2025-10-07 13:04:48 -0400

    feat: models for Service and Technician

--- Models/Technician.cs
+++ Models/Technician.cs
@@ -2,5 +2,16 @@ namespace FuerzaG.Models;
 
 public class Technician
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string FirstLastname { get; set; } = string.Empty;
+    public string? SecondLastname { get; set; }
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Ci { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public decimal BaseSalary { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsActive { get; set; }
 }
